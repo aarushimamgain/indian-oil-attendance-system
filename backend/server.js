a0fdc780174c2a0ee947bfd49e20b414452d5728
Aarushi Mamgain <aarushimamgain2005@gmail.com>
2026-09-01 00:54:12 +0530

    url added

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,10 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.json({ message: "Attendance API is running" });
+});
+
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
@@ -181,7 +185,7 @@ function addMinutesToTime(time, minsToAdd) {
   return `${newH}:${newM}`;
 }
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server running at http://localhost:${PORT}`);
 });
